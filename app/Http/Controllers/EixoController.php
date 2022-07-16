b0c6be151afefaaa800185634528961a1e5021bc
Bruna Leticia <bruna.letavlis@outlook.com>
2022-07-16 12:08:50 -0300

    edit eixo ok

--- app/Http/Controllers/EixoController.php
+++ app/Http/Controllers/EixoController.php
@@ -45,32 +45,36 @@ class EixoController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
-    {
-        //
+    public function show($id){
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
+    
+    public function edit($id){
+        $data = Eixo::find($id);
+        if(!isset($data)){return"<h1>ID: $id não encontrado!</h1>";}
+        return view('eixos.edit', compact('data'));
+    
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
+    public function update(Request $request, $id){
+        $regras =[
+            'nome' => 'required|max:50|min:10',
+        ];
+
+        $msgs =[
+            "required" => "O preenchimento do campo [:attribute] é obrigatório!",
+            "max" => "O campo [:attribute] possui tamanho máximo de [:max] caracteres!",
+            "min" => "O campo [:attribute] possui tamanho mínimo de [:min] caracteres!",
+        ];
+        $request->validate($regras, $msgs);
+        $obj = Eixo::find($id);
+        $obj->fill([
+            'nome' => mb_strtoupper($request->nome, 'UTF-8')
+        ]);
+
+        $obj->save();
+
+        return redirect()->route('eixos.index');
     }
 
     /**
