92f5749299a8c552144a6630fbc5621b1520c843
Bruna Leticia <bruna.letavlis@outlook.com>
2022-07-16 10:55:39 -0300

    Initial

---
+++ database/migrations/2022_07_16_134441_eixo.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Eixo extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('eixo', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('eixo');
+    }
+}
